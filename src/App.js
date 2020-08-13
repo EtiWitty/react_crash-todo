e2288deb7d116645e5f1904f1e478d00d61bd45f
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-08-13 11:46:51 -0700

    implemented AddTodo component and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
-import Todos from './components/Todos';
+import React, { Component } from 'react'
+import Todos from './components/Todos'
+import Header from './components/layout/Header'
+import AddTodo from './components/AddTodo'
 
 import './App.css';
 
@@ -47,11 +49,15 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Todos 
-          todos={ this.state.todos } 
-          markComplete= { this.markComplete }
-          delTodo = { this.delTodo }
-        />
+        <div className="container">
+          <Header />
+          <AddTodo />
+          <Todos 
+            todos={ this.state.todos } 
+            markComplete= { this.markComplete }
+            delTodo = { this.delTodo }
+          />
+        </div>
       </div>
     );
   }
