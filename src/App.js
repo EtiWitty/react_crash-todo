fcee48d588baeedb1d7163387d8e32e7e50f9d95
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-08-13 10:35:00 -0700

    impelemented todo button and its functionality

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
     ]
   }
 
+  //Toggle Complete
   markComplete = (id) => {
     this.setState({
       todos: this.state.todos.map((todo) => {
@@ -35,11 +36,22 @@ class App extends Component {
     });
   };
 
+  //Delete Todo
+  delTodo = (id) => {
+    this.setState ({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  }
+
   render() {
 
     return (
       <div className="App">
-        <Todos todos={ this.state.todos } markComplete= {this.markComplete}/>
+        <Todos 
+          todos={ this.state.todos } 
+          markComplete= { this.markComplete }
+          delTodo = { this.delTodo }
+        />
       </div>
     );
   }
